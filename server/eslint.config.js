c2fd4d3e2a5102fb27ff16ee183765973499398a
Adam Bennett <agbwebdesigns@gmail.com>
2026-06-28 13:11:47 -0700

    init commit, setup client and server bootstraps, setup eslint and prettier

---
+++ server/eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+
+export default tseslint.config(
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  prettier,
+  {
+    files: ["src/**/*.ts"],
+    languageOptions: {
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          argsIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
+      ],
+      "@typescript-eslint/no-explicit-any": "warn",
+    },
+  },
+);
